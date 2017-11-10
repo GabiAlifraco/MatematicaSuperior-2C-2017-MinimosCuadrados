4673ee73a126fc7786370fd5ac59f8881c321c89
Jonathan <jvictoricacrescente@est.frba.utn.edu.ar>
2017-11-10 00:19:51 -0300

    ultimosa

--- Superior/src/main/java/com/superior/controller/main.java
+++ Superior/src/main/java/com/superior/controller/main.java
@@ -1,20 +1,19 @@
 package com.superior.controller;
 
-import com.superior.model.dto.AproxTable;
-import com.superior.view.AproximacionPantalla;
+import com.superior.view.PantallaPrincipal;
 
 public class main {
 
 	public static void main(final String[] args) {
 
-		 AproxTable aprox = new AproxTable(3);
-		 aprox.agregarPunto(1, 7);
-		 aprox.agregarPunto(2, 30);
-		 aprox.agregarPunto(3, 90);
-		 aprox.agregarPunto(4, 170);
-		 aprox.agregarPunto(5, 290);
-		 aprox.agregarPunto(6, 450);
-		 aprox.agregarPunto(7, 650);
+//		 AproxTable aprox = new AproxTable(3);
+//		 aprox.agregarPunto(1, 7);
+//		 aprox.agregarPunto(2, 30);
+//		 aprox.agregarPunto(3, 90);
+//		 aprox.agregarPunto(4, 170);
+//		 aprox.agregarPunto(5, 290);
+//		 aprox.agregarPunto(6, 450);
+//		 aprox.agregarPunto(7, 650);
 		//
 		
 //		 aprox.agregarPunto(1, 5.1);
@@ -25,8 +24,8 @@ public class main {
 ////		 aprox.agregarPunto(6, 450);
 ////		 aprox.agregarPunto(7, 650);
 		
-		new AproximacionPantalla(aprox, 3);
-//		new PantallaPrincipal();
+//		new AproximacionPantalla(aprox, 3);
+		new PantallaPrincipal();
 	}
 
 }
